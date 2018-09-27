28686eb7bed47956a1777eb2e6b7883bcbc02e7b
growabeard <chris.w.witt@gmail.com>
2018-09-27 16:32:05 -0400

    Updating monitor arduino code and adding code for attiny85 transmitter so I don't lose it.

--- temp-monitor/temp-monitor.ino
+++ temp-monitor/temp-monitor.ino
@@ -1,32 +1,76 @@
+#include <Adafruit_Sensor.h>
+
+#include <DHT_U.h>
+#include <DHT.h>
+
 // include the library code:
 #include <LiquidCrystal.h>
-#include <dht_nonblocking.h>
 
-#define DHT_SENSOR_TYPE DHT_TYPE_11
+#define DHTTYPE DHT11
+
+int photocellPin = A5;     // the cell and 10K pulldown are connected to a0
+int photocellReading;     // the analog reading from the sensor divider
+
+int val = 0; //value for storing moisture value 
+int soilPin = A0;//Declare a variable for the soil moisture sensor 
+int soilPower = 6;//Variable for Soil moisture Power
 
 
 static const int DHT_SENSOR_PIN = 2;
-DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
+DHT dht( DHT_SENSOR_PIN, DHTTYPE );
 
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 
 void setup() {
+  dht.begin();
   Serial.begin( 9600);
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
-  // Print a message to the LCD.
+
+  pinMode(soilPower, OUTPUT);//Set D7 as an OUTPUT
+  digitalWrite(soilPower, LOW);//Set to LOW so no power is flowing through the sensor
+
+  delay(500);
 }
 
 void loop() {
+  Serial.print("Reading");
+  lcd.setCursor(0,1);
+  lcd.print("L " + String(getPhotocell()) + " S " + String(readSoil()));
 
+  String tempHumid = getTempAndHumidity();
+
+  lcd.setCursor(0,0);
+  lcd.print(tempHumid);
+  delay(10000);
+}
+
+int getPhotocell() {
+  photocellReading = analogRead(photocellPin);
+
+  return map(photocellReading, 0, 1023, 0, 100);
+}
+
+String getTempAndHumidity() {
   float temperature;
   float humidity;
+  String temp = "no ";
 
-  if(dht_sensor.measure(&temperature, &humidity)){
-    String tempF = String(temperature * 1.8 + 32);
-    lcd.setCursor(0,0);
-    String temp = "T " + tempF + "* H " + String(humidity) + "%";
-    lcd.print(temp);
-  }
+  temperature = dht.convertCtoF(dht.readTemperature());
+  humidity = dht.readHumidity();
+  
+  temp = "T " + String(temperature) + "* H " + String(humidity) + "%";
+  
+  return temp;
+}
+
+int readSoil()
+{
+
+    digitalWrite(soilPower, HIGH);//turn D7 "On"
+    delay(10);//wait 10 milliseconds 
+    val = analogRead(soilPin);//Read the SIG value form sensor 
+    digitalWrite(soilPower, LOW);//turn D7 "Off"
+    return val;//send current moisture value
 }
