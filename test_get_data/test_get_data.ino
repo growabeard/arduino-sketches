c4d32ede023044853f1570774896f803fa7735e8
growabeard <chris.w.witt@gmail.com>
2018-10-22 19:10:06 -0400

    moving to a different library

--- test_get_data/test_get_data.ino
+++ test_get_data/test_get_data.ino
@@ -1,193 +1,232 @@
-/************************************************************
-ESP8266_Shield_Demo.h
-SparkFun ESP8266 AT library - Demo
-Jim Lindblom @ SparkFun Electronics
-Original Creation Date: July 16, 2015
-https://github.com/sparkfun/SparkFun_ESP8266_AT_Arduino_Library
+#include <LiteESP8266Client.h>
 
-This example demonstrates the basics of the SparkFun ESP8266
-AT library. It'll show you how to connect to a WiFi network,
-get an IP address, connect over TCP to a server (as a client),
-and set up a TCP server of our own.
+// Uncomment this if you have the LiteSerialLogger library installed.  This will
+// save about 200 bytes of SRAM.
+//#define USE_LITE_SERIAL
 
-Development environment specifics:
-  IDE: Arduino 1.6.5
-  Hardware Platform: Arduino Uno
-  ESP8266 WiFi Shield Version: 1.0
+// If LiteSerialLogger is installed, use it - otherwise just use the stock
+// Serial library.
+#ifdef USE_LITE_SERIAL
+#include <LiteSerialLogger.h>
+#define LOGGER LiteSerial
+#else
+#define LOGGER Serial
+#endif
 
-This code is released under the MIT license.
+// Radio!
+LiteESP8266 radio;
 
-Distributed as-is; no warranty is given.
-************************************************************/
+// Set your SSID and password here, obviously.
+// If you don't have a password, you should be able to leave it blank.
+const char ssid[] PROGMEM = "butts";
+const char password[] PROGMEM = "butts";
 
-//////////////////////
-// Library Includes //
-//////////////////////
-// SoftwareSerial is required (even you don't intend on
-// using it).
-#include <SoftwareSerial.h> 
-#include <SparkFunESP8266WiFi.h>
-
-//////////////////////////////
-// WiFi Network Definitions //
-//////////////////////////////
-// Replace these two character strings with the name and
-// password of your WiFi network.
-const char mySSID[] = "GM Guest WiFi";
-const char myPSK[] = "";
-
-
-//////////////////
-// HTTP Strings //
-//////////////////
-const char destServer[] = "monitree.herokuapp.com";
-
-const String httpRequest = "GET /monitree/v1/readings/13 HTTP/1.1\r\n"
-                           "Host: monitree.herokuapp.com\r\n"
-                           "Content-Type: application/json\r\n\r\n";
-                           
-// All functions called from setup() are defined below the
-// loop() function. They modularized to make it easier to
-// copy/paste into sketches of your own.
-void setup() 
-{
-  // Serial Monitor is used to control the demo and view
-  // debug information.
-  Serial.begin(9600);
-
-  // initializeESP8266() verifies communication with the WiFi
-  // shield, and sets it up.
-  initializeESP8266();
-
-  // connectESP8266() connects to the defined WiFi network.
-  connectESP8266();
-
-  // displayConnectInfo prints the Shield's local IP
-  // and the network it's connected to.
-  displayConnectInfo();
+/**
+ *This is a test host I use - see below for the PHP scripts running on it.
+ * If you don't have it, you can still see how things work and use the code for
+ * your own project, but this sample won't work.
+ * 
+ * test/get_bytes.php
+<?php
+if (isset($_GET['bytes'])) {
+  $count = $_GET['bytes'];
+} else {
+  $count = 100;
 }
 
-void loop() 
-{
-  clientDemo();
-  delay(50000);
+for ($i = 0; $i < $count; $i++) {
+  print ($i % 10);
 }
+?>
 
-void initializeESP8266()
-{
-  // esp8266.begin() verifies that the ESP8266 is operational
-  // and sets it up for the rest of the sketch.
-  // It returns either true or false -- indicating whether
-  // communication was successul or not.
-  // true
-  int test = esp8266.begin(9600);
-  if (test != true)
-  {
-    Serial.println(F("Error talking to ESP8266."));
-  }
-  Serial.println(F("ESP8266 Shield Present"));
-}
+ * test/post_echo.php:
+<?php
+print_r($_POST);
+?>
+ */
+const char host[] PROGMEM = "monitree.herokuapp.com";
 
-void connectESP8266()
-{
-  // The ESP8266 can be set to one of three modes:
-  //  1 - ESP8266_MODE_STA - Station only
-  //  2 - ESP8266_MODE_AP - Access point only
-  //  3 - ESP8266_MODE_STAAP - Station/AP combo
-  // Use esp8266.getMode() to check which mode it's in:
-  int retVal = esp8266.getMode();
-  if (retVal != ESP8266_MODE_STA)
-  { // If it's not in station mode.
-    // Use esp8266.setMode([mode]) to set it to a specified
-    // mode.
-    retVal = esp8266.setMode(ESP8266_MODE_STA);
-    if (retVal < 0)
-    {
-      Serial.print(F("Error setting mode. "));
-      Serial.println(retVal);
-    }
-  }
-  Serial.println(F("Mode set to station"));
+/**
+ * Sending HTTP GET and POST request types should send proper headers.
+ * 
+ * For a basic GET, you'll send something like this.  Note that the lines are
+ * all CRLF terminated, with a double CRLF after the last line of the request.
+ * 
+ * GET /test/get_bytes.php?bytes=500 HTTP/1.0\r\n
+ * User-Agent: SyonykArduino/0.1\r\n
+ * Connection: close\r\n\r\n
+ * 
+ * To make a POST, you want to do something like this.  Note the double CRLF
+ * before the actual POST data (which is remarkably simple).
+ *
+ * POST /test/post_echo.php HTTP/1.0\r\n
+ * User-Agent: SyonykArduino/0.1\r\n
+ * Content-Type: application/x-www-form-urlencoded\r\n
+ * Content-length: 28\r\n
+ * Connection: close\r\n\r\n
+ * foo=1&bar=2&baz=asdfasdfasdf
+ */
 
-  // esp8266.status() indicates the ESP8266's WiFi connect
-  // status.
-  // A return value of 1 indicates the device is already
-  // connected. 0 indicates disconnected. (Negative values
-  // equate to communication errors.)
-  retVal = esp8266.status();
-  if (retVal <= 0)
-  {
-    Serial.print(F("Connecting to "));
-    Serial.println(mySSID);
-    // esp8266.connect([ssid], [psk]) connects the ESP8266
-    // to a network.
-    // On success the connect function returns a value >0
-    // On fail, the function will either return:
-    //  -1: TIMEOUT - The library has a set 30s timeout
-    //  -3: FAIL - Couldn't connect to network.
-    retVal = esp8266.connect(mySSID, myPSK);
-    if (retVal < 0)
-    {
-      Serial.print(F("Error connecting "));
-      Serial.println(retVal);
-    }
-  }
-}
+// Opening line for a GET of 500 bytes from the test host.
+const char http_get_500_bytes_request[] PROGMEM = 
+    "GET /monitree/v1/readings/14 HTTP/1.1\r\n";
 
-void displayConnectInfo()
-{
-  char connectedSSID[24];
-  memset(connectedSSID, 0, 24);
-  // esp8266.getAP() can be used to check which AP the
-  // ESP8266 is connected to. It returns an error code.
-  // The connected AP is returned by reference as a parameter.
-  int retVal = esp8266.getAP(connectedSSID);
-  if (retVal > 0)
-  {
-    Serial.print(F("Connected to: "));
-    Serial.println(connectedSSID);
-  }
+// Opening line of a GET request to the default page on a host.
+const char http_get_index[] PROGMEM = "GET / HTTP/1.0\r\n";
 
-  // esp8266.localIP returns an IPAddress variable with the
-  // ESP8266's current local IP address.
-  IPAddress myIP = esp8266.localIP();
-  Serial.print(F("My IP: ")); Serial.println(myIP);
-}
+// HTTP POST header strings.
+const char http_post_request[] PROGMEM =
+        "POST /monitree/v1/readings HTTP/1.1\r\n";
+const char http_post_content_type[] PROGMEM = 
+        "Content-Type: application/json\r\n";
+const char http_post_content_length[] PROGMEM = "Content-length: ";
+const char http_post_crlf[] PROGMEM = "\r\n";
 
-void clientDemo()
-{
-  // To use the ESP8266 as a TCP client, use the 
-  // ESP8266Client class. First, create an object:
-  ESP8266Client client;
+// Actual POST data (this is how a form gets encoded).
+const char http_post_content[] PROGMEM = "{\"id\": 30, \"name\": \"tree\", \"date\": \"\", \"temp\": 77, \"creator\": \"sensor\", \"moisture\": 89, \"humidity\": 45, \"light\": 76, \"watered\": false }";
+
+// Common headers.  Note that the close_connection string has a double CRLF.
+const char http_useragent[] PROGMEM = "User-Agent: Monitree/0.1\r\n";
+const char http_close_connection[] PROGMEM = "Connection: close\r\n\r\n";
+
+// For testing a get to google.com - store the hostname in program memory.
+const char google_domain[] PROGMEM = "www.google.com";
 
 
-//  int16_t resp = client.ping("monitree.herokuapp.com");
-//  Serial.print("Ping " + String(resp));
-  // ESP8266Client connect([server], [port]) is used to 
-  // connect to a server (const char * or IPAddress) on
-  // a specified port.
-  // Returns: 1 on success, 2 on already connected,
-  // negative on fail (-1=TIMEOUT, -3=FAIL).
-  int retVal = client.connect(destServer, 80);
-  if (retVal <= 0)
-  {
-    Serial.print(F("Failed to connect to server."));
-    Serial.println(retVal);
+// Get (and print) whatever it is google.com is serving up.
+void make_http_get_multiple_packets() {
+  char *data;
+
+  LOGGER.println(F("Trying to fetch http://www.google.com/..."));
+  
+  // Connect (or try to).
+  if (radio.connect_progmem(google_domain, 80)) {
+    LOGGER.println(F("Connect (to Google) success."));
+  } else {
+    LOGGER.println(F("Connect (to Google) failure."));
     return;
   }
-  Serial.print(httpRequest);
-  // print and write can be used to send data to a connected
-  // client connection.
-  client.print(httpRequest);
 
-  delay(5000);
-  // available() will return the number of characters
-  // currently in the receive buffer.
-  while (client.available())
-    Serial.write(client.read()); // read() gets the FIFO char
-  
-  // connected() is a boolean return value - 1 if the 
-  // connection is active, 0 if it's closed.
-  if (client.connected())
-    client.stop(); // stop() closes a TCP connection.
+  // Send the proper headers in order.  Note that http_close_connection already
+  // has the double CRLF so has to be sent last.
+  radio.send_progmem(http_get_index);
+  radio.send_progmem(http_useragent);
+  radio.send_progmem(http_close_connection);
+
+  // The largest response I've seen "in the wild" is 1460 bytes - packet size.
+  // Getting 1460 bytes of data at 9600 baud takes about 1.5 seconds.
+  // Give it 10s, because it can take a while to push everything through.
+  // This will wait 10s after the last packet, as it cannot tell the data stream
+  // is done other than by not seeing any more responses.
+  while (data = radio.get_response_packet(1500, 10000)) {
+    if (data) {
+      LOGGER.print(F("Got response packet of size: "));
+      LOGGER.println(strlen(data));
+      free(data);
+    } else {
+      // This should not happen.
+      LOGGER.println(F("Error: Data came back null."));
+    }
+  }
 }
+
+// Get a blob of data and parse it as a HTTP response.
+void make_http_get() {
+  char *data;
+  LOGGER.println(F("Requesting 500 bytes from get_bytes.php..."));
+  
+  //  ==== Test 500 bytes ====
+  if (radio.connect_progmem(host, 80)) {
+    LOGGER.println(F("Connect success."));
+  } else {
+    LOGGER.println(F("Connect failure."));
+  }
+
+  radio.send_progmem(http_get_500_bytes_request);
+  radio.send_progmem(http_useragent);
+  radio.send_progmem(http_close_connection);
+
+  data = radio.get_http_response(512);
+  
+  if (data) {
+    LOGGER.print(F("Expected 500 bytes, got: "));
+    LOGGER.println(strlen(data));
+    LOGGER.print(data);
+    free(data);
+  } else {
+    LOGGER.println(F("Error: Data came back null."));
+    return false;
+  }
+}
+
+// HTTP POST - as a form upload.
+void make_http_post() {
+  char *data;
+  char content_length_ascii[8];
+
+  LOGGER.println(F("Sending test POST to post_echo.php..."));
+  
+  if (radio.connect_progmem(host, 80)) {
+    LOGGER.println(F("Connect success."));
+  } else {
+    LOGGER.println(F("Connect failure."));
+  }
+
+  radio.send_progmem(http_post_request);
+  radio.send_progmem(http_useragent);
+  radio.send_progmem(http_post_content_type);
+  radio.send_progmem(http_post_content_length);
+  // Insert the content length.
+  itoa(strlen_P(http_post_content), content_length_ascii, 10);
+  radio.send(content_length_ascii);
+  radio.send_progmem(http_post_crlf);
+  radio.send_progmem(http_close_connection);
+  radio.send_progmem(http_post_content);
+
+  data = radio.get_http_response(512);
+  if (data) {
+    LOGGER.println(F("Got response:"));
+    LOGGER.println(data);
+    free(data);
+  } else {
+    LOGGER.println(F("Error: Data came back null."));
+    return false;
+  }
+}
+
+
+void setup() {
+  // Don't "double start" after code is uploaded.  It confuses the radio.
+  delay(2000);
+    
+  // Radio is set, on my hardware, to 9600 baud, on the default pins.
+  radio.begin(115200);
+  LOGGER.begin(9600);
+  
+  // If the radio is already in station mode, there's no need to do this every
+  // time.
+  if (radio.set_station_mode()) {
+    LOGGER.println(F("Set station mode success."));
+  } else {
+    LOGGER.println(F("Set station mode failed."));
+  }
+
+  if (radio.connect_to_ap(ssid, password)) {
+    LOGGER.println(F("Join AP success."));
+  } else {
+    LOGGER.println(F("Join AP failed."));
+  }
+
+  // Test HTTP get and POST
+  make_http_get();
+  make_http_get_multiple_packets();
+  make_http_post();
+
+  LOGGER.println(F("Done with test code!"));
+}
+
+void loop() {
+  // No code in loop.
+}
+
+
